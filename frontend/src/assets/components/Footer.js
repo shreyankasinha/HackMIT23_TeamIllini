f0d26fa28b590cea8e824c2e2f9dc198cb957eb8
neilteje <neilteje@gmail.com>
2023-09-17 06:45:54 -0400

    frontend 6am work

---
+++ frontend/src/assets/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="App-footer">
+      <p>Powered by HackMIT</p>
+    </footer>
+  );
+}
+
+export default Footer;
